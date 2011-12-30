d43513fc1b811956a2eaebd2cc526e9adf5c26ed
Michael Lavelle <michael@lavelle.name>
2011-12-30 21:23:23 +0000

    Initial commit

---
+++ src/main/java/org/socialsignin/provider/linkedin/LinkedInProviderConfig.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.socialsignin.provider.linkedin;
+
+import org.socialsignin.provider.AbstractProviderConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.social.connect.web.ConnectInterceptor;
+import org.springframework.social.linkedin.api.LinkedIn;
+import org.springframework.social.linkedin.connect.LinkedInConnectionFactory;
+
+/** 
+* @author Michael Lavelle
+*/
+@Configuration
+public class LinkedInProviderConfig extends AbstractProviderConfig<LinkedIn> {
+
+	@Autowired
+	private LinkedInConnectInterceptor linkedInConnectInterceptor;
+
+	@Value("${linkedin.apiKey}")
+	private String linkedInClientId;
+
+	@Value("${linkedin.secretKey}")
+	private String linkedInClientSecret;
+
+	@Override
+	protected ConnectionFactory<LinkedIn> createConnectionFactory() {
+		return new LinkedInConnectionFactory(linkedInClientId, linkedInClientSecret);
+	}
+
+	@Override
+	protected ConnectInterceptor<LinkedIn> getConnectInterceptor() {
+		return linkedInConnectInterceptor;
+	}
+
+}
